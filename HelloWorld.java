69b75d7a25056d6a34ce61238f4232ab8dd812bc
GattuPavani <gattupavani09@gmail.com>
2025-07-17 21:28:07 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public classHelloWorld{
-	public static void main(String[] args){
-		System.out.println("Hello from Jenkins CI/CD!");
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI/CD!");
+    }
 }
